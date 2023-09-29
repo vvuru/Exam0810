79435c6e3fdaed7291a3a017f17237f676579ad0
vvuru <wown5398@gmail.com>
2023-09-29 18:39:44 +0900

    Initial commit

---
+++ src/Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
