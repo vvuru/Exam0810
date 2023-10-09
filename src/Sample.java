78435715dc94a9aab7eca169a511a9bd2c1afcda
vvuru <wown5398@gmail.com>
2023-10-09 12:26:22 +0900

    var_life

--- src/Sample.java
+++ src/Sample.java
@@ -1,10 +1,15 @@
 public class Sample {
+    int outbox1 = 7;
+    static int outbox2 = 5;
+
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("Hello");
-        sb.append(" ");
-        sb.append("World");
-        String res = sb.toString();
-        System.out.println(res);
+        int box1 = 5;
+        {
+            int box2 = 2;
+            System.out.println(box1);
+            System.out.println(box2);
+        }
+//        System.out.println(box2);
+//        System.out.println(outbox1);
     }
 }
